c358e9a00040486d36f1ff18461255985e6dc86e
Dan Fortinho <fortinhodanvhamba@gmail.com>
2024-12-05 12:35:50 +0200

    fix somE bug

--- src/pages/utilisateur/ListUtilisateur.jsx
+++ src/pages/utilisateur/ListUtilisateur.jsx
@@ -23,7 +23,7 @@ const ListUtilisateur = () => {
         {/* Left: Title */}
         <div className="mb-4 sm:mb-0">
           <h1 className="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">
-            Liste des Utilisateurs
+            LISTE DES UTILISATEURS
           </h1>
         </div>
 
